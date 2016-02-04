0aa9e70f9c057102acaec25ce6fa42b23b994189
Nathaniel Baer <njbbaer@gmail.com>
2016-02-03 19:33:35 -0500

    Created framework for importing AIMind files

--- app/engine.js
+++ app/engine.js
@@ -191,6 +191,11 @@ var commands = {
   'Unmute': function() { Voice.unmute(); },
   'Export AIMind *filename': function(filename) {
     AIMind.export(Knowledge.getGraph(), filename);
+    showVoiceAndConsoleResponse('Exported AIMind as ' + filename);
+  },
+  'Import AIMind *filename': function(filename) {
+    AIMind.import(filename);
+    showVoiceAndConsoleResponse('Imported AIMind from ' + filename);
   },
   'Grow *keyword': handleGrow
 };
